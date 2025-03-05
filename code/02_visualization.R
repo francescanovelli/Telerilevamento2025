f37f55727f1c0aa5363c2fee7e41565379260afc
francescanovelli <novellifrancescaa@gmail.com>
2025-03-05 17:40:54 +0100

    02_visualization.R

--- code/02_visualization.R
+++ code/02_visualization.R
@@ -1,1 +1,20 @@
+# code for visualizing satellite data
 
+library(terra)
+library(imageRy)
+
+# lista dei file
+im.list()
+
+# importare i dati
+im.import()
+b2 <- im.import("sentinel.dolomites.b2.tif")
+
+# per far cambiare i colori alla foto uso la funzione 
+colorRampPalette()
+cl <- colorRampPalette(c("black","dark grey", "light grey"))(100)
+
+# in questo modo coloro la mia immagine con altri colori
+plot(b2, col=cl)
+
+# ES: fabbrico la mia gamma di colori
