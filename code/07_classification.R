63816a39cd5000587b7d2ce4ca6d4fb12aeb4be0
francescanovelli <novellifrancescaa@gmail.com>
2025-04-03 14:19:51 +0200

    Create 07_classification.R

---
+++ code/07_classification.R
@@ -0,0 +1,57 @@
+# i codici di R per classificare le immagini
+
+library(terra)
+library(imageRy)
+library(ggplot2)
+library(patchwork)
+
+im.list()
+
+mato1992 = im.import("matogrosso_l5_1992219_lrg.jpg")
+mato1992 = flip(mato1992)
+plot(mato1992)
+
+mato2006 = im.import("matogrosso_ast_2006209_lrg.jpg")
+mato2006 = flip(mato2006)
+plot(mato2006)
+
+mato1992c = im.classify(mato1992, num_clusters=2)
+# classe 1 - Antropica
+# classe 2 - Foresta
+
+mato2006c = im.classify(mato2006, num_clusters=2)
+# classe 1 - Foresta
+# classe 2 - Antropica
+
+# le classi come colore vengono assegate in maniera randomica
+
+# si possono calcolare le frequenze nell'immagine, cioè il numero di volte che quella classe compare nel pixel
+f1992 = freq(mato1992c)
+f1992
+
+# calcoliamo una proporzione
+tot1992 = ncell(mato1992c) # calcolo il tot. dei pixel
+prop1992 = f1992/tot1992
+  # 16.9% per la classe 1
+  # 83.1% per la classe 2
+
+# per l'immagine del 2006 le percentuali sono 
+  # 54.7% per la classe 1
+  # 45.3% per la classe 2
+
+# Creiamo una tabella
+class = c("Antropico","Foresta")
+y1992 = c(17, 83)
+y2006 = c(55, 45)
+tab = data.frame(class, y1992, y2006)
+
+# Creiamo un grafico, usando la library di ggplot2
+p1 = ggplot(tab, aes(x=class, y=y1992, color=class)) + 
+  geom_bar(stat="identity", fill="white") +
+  ylim(c(0,100)) + coord_flip()
+
+p2 = ggplot(tab, aes(x=class, y=y2006, color=class)) + 
+  geom_bar(stat="identity", fill="white") +
+  ylim(c(0,100)) + coord_flip()
+
+p1/p2
