0754b082c18740d0336702630e8668b5ef200035
francescanovelli <novellifrancescaa@gmail.com>
2025-04-09 17:26:53 +0200

    Update 08_spatial_variability.R

--- code/08_spatial_variability.R
+++ code/08_spatial_variability.R
@@ -2,6 +2,8 @@
 
 library(terra)
 library(imageRy)
+library(viridis)
+library(ggplot2)
 
 # Deviazione Standard 
 
@@ -34,8 +36,25 @@ im.plotRGB(sent, r=2, g=1, b=3) # Plotto l'immagine di prima con il NIR al posto
 im.plotRGB(sent, r=2, g=3, b=1) # Plotto l'immagine di prima con il NIR al posto del blu, in RGB
 # le parti dove si mette in NIR è la vegetazione, la parte nera è l'acqua, il bianco è neve o nuvole
 
+nir = sent[[1]]
+plot(nir, col= inferno(100))
 
+# focal() serve per le finestre che si muovono e la usaimo per calcolare la deviazione standard
 
+sd3 = focal(nir, w=c(3,3), fun="sd")
+plot(sd3) # la deviazione standard su un matrice 3x3, notiamo tutte le zone in cui c'è stata una variazione del NIR
 
+im.multiframe(1,2)
+im.plotRGB(sent, r=1, g=2, b=3) 
+plot(sd3)
+
+sd5 = focal(nir, w=c(5,5), fun="sd")
+plot(sd5) # ho zone con più ampia variabilità perchè sto espandendo la finestra di calcolo
+
+im.multiframe(1,2)
+plot(sd3)
+plot (sd5)
+
+im.ggplot(sd3)
 
 
