f6a4cd34cf2aebd6d4ec5fef3b5e8146725d6080
francescanovelli <novellifrancescaa@gmail.com>
2025-06-29 22:21:34 +0200

    Update foglio_r.R

--- esame/foglio_r.R
+++ esame/foglio_r.R
@@ -8,7 +8,7 @@ setwd("C:/Users/User/OneDrive/Documenti/UNI/TELERILEVAMENTO")
 im.multiframe(1,3)
 
 # import raster
-Sentinel_NZ25 <- rast("NZ_median_2025.tif")
+Sentinel_NZ25 <- rast("NZ_NIR25.tif")
 Sentinel_NZ25
 
 # visualize RGB (check the order of the bands)
@@ -16,7 +16,7 @@ plotRGB(Sentinel_NZ25, r = 1, g = 2, b = 3, stretch = "lin", main = "Sentinel (m
 
 Sentinel_NZ19 <- rast("NZ_NIR19.tif")
 Sentinel_NZ19
-Sentinel_NZ20 <- rast("NZ_median_2020.tif")
+Sentinel_NZ20 <- rast("NZ_NIR20.tif")
 Sentinel_NZ20
 
 plotRGB(Sentinel_NZ19, r = 1, g = 2, b = 3, stretch = "lin", main = "Sentinel (median), 2019")
