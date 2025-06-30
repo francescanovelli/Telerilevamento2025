f74be6b2d99e627a810b1c26960e7f2a4fc43c7f
francescanovelli <novellifrancescaa@gmail.com>
2025-06-30 14:02:05 +0200

    Update foglio_r.R

--- esame/foglio_r.R
+++ esame/foglio_r.R
@@ -3,21 +3,26 @@
 library(terra)
 library(imageRy)
 
+# Inserisco la directory
 setwd("C:/Users/User/OneDrive/Documenti/UNI/TELERILEVAMENTO")
 
+# Apro un pannello multiframe
 im.multiframe(1,3)
 
-# import raster
+# importo i raster delle 3 immagini scaricate da GEE
 Sentinel_NZ25 <- rast("NZ_NIR25.tif")
 Sentinel_NZ25
 
-# visualize RGB (check the order of the bands)
-plotRGB(Sentinel_NZ25, r = 1, g = 2, b = 3, stretch = "lin", main = "Sentinel (median), 2025")
-
 Sentinel_NZ19 <- rast("NZ_NIR19.tif")
 Sentinel_NZ19
+
 Sentinel_NZ20 <- rast("NZ_NIR20.tif")
 Sentinel_NZ20
 
+
+# visualizzo le immagini in RGB 
+plotRGB(Sentinel_NZ25, r = 1, g = 2, b = 3, stretch = "lin", main = "Sentinel (median), 2025")
+
 plotRGB(Sentinel_NZ19, r = 1, g = 2, b = 3, stretch = "lin", main = "Sentinel (median), 2019")
+
 plotRGB(Sentinel_NZ20, r = 1, g = 2, b = 3, stretch = "lin", main = "Sentinel (median), 2020")
