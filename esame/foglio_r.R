66e4a12eee453c5b458d5189400232d5d709f9a9
francescanovelli <novellifrancescaa@gmail.com>
2025-06-30 14:33:15 +0200

    Update foglio_r.R

--- esame/foglio_r.R
+++ esame/foglio_r.R
@@ -11,15 +11,14 @@ setwd("C:/Users/User/OneDrive/Documenti/UNI/TELERILEVAMENTO")
 im.multiframe(1,3)
 
 # importo i raster delle 3 immagini scaricate da GEE
-Sentinel_NZ25 <- rast("NZ_NIR25.tif")
-Sentinel_NZ25
-
 Sentinel_NZ19 <- rast("NZ_NIR19.tif")
 Sentinel_NZ19
 
 Sentinel_NZ20 <- rast("NZ_NIR20.tif")
 Sentinel_NZ20
 
+Sentinel_NZ25 <- rast("NZ_NIR25.tif")
+Sentinel_NZ25
 
 # visualizzo le immagini in RGB 
 plotRGB(Sentinel_NZ19, r=1, g=2, b=3, stretch= "lin", main= "Sentilenl-2, Mag. 2019")
