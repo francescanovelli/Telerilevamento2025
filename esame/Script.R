1ffefbf9dea74c924b487cc09a9230fe5cd5209b
francescanovelli <novellifrancescaa@gmail.com>
2025-07-02 12:29:01 +0200

    Update Script.R

--- esame/Script.R
+++ esame/Script.R
@@ -37,9 +37,9 @@ plotRGB(Sentinel_NZ25, r=1, g=2, b=3, stretch= "lin", main= "Sentilenl-2, Mag. 2
 
 # visualizzo le immagini con la banda del NIR al posto della banda del verde
 im.multiframe(1,3)
-im.plotRGB(Sentinel_NZ19, r=1, g=4, b=2)
-im.plotRGB(Sentinel_NZ20, r=1, g=4, b=2)
-im.plotRGB(Sentinel_NZ25, r=1, g=4, b=2)
+plotRGB(Sentinel_NZ19, r=1, g=4, b=2, stretch= "lin", main= "Sentilenl-2, Mag. 2019")
+plotRGB(Sentinel_NZ20, r=1, g=4, b=2, stretch= "lin", main= "Sentilenl-2, Mag. 2020")
+plotRGB(Sentinel_NZ25, r=1, g=4, b=2, stretch= "lin", main= "Sentilenl-2, Mag. 2025")
 
 # Calcolo NDVI, e creo un immagine con tutti e 3 gli NDVI   
 ndvi19= im.ndvi(Sentinel_NZ19, 4, 1)
